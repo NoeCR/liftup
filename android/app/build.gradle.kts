8709e6c8977b17ff2660a0e22438d1d6c0258f45
noecr.dev@gmail.com <noecr.dev@gmail.com>
2025-10-06 18:47:24 +0200

    fix(android): alinear paquete y Activity con com.noecr.liftly y evitar restaura automática

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.noecr.liftly"
+    namespace = "com.example.liftup"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -18,8 +18,9 @@ android {
     kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.noecr.liftly"
+        // TODO: Specify your own unique Application ID
+        // (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.liftup"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
