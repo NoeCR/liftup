fa7a3df5ca6b922d509edb766e2424305416f674
noecr.dev@gmail.com <noecr.dev@gmail.com>
2025-10-06 18:44:59 +0200

    fix(android): asegurar namespace/applicationId com.noecr.liftly (se revertía al guardar)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.liftup"
+    namespace = "com.noecr.liftly"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -18,9 +18,8 @@ android {
     kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID
-        // (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.liftup"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.noecr.liftly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
